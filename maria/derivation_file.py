d208f1686b88883e4930f8fe853e348926d24fc7
Maria Alexa <maria.alexa99@gmail.com>
2022-10-14 22:20:08 +0200

    s derivated according to theta

--- maria/derivation_file.py
+++ maria/derivation_file.py
@@ -1,27 +1,31 @@
+#N number of assets
 
-
-#must define A, B and S in function of T
-def s_derivated_calculation(mu, delta, T, theta):
-    x=x(T)
-    A, B = R_calculation(mu, delta, r, F, T)
-    S=A/ math.sqrt(B-A^2)
-    s_derivated=0
-    F_derivated=0
+def s_derivated_calculation(N, mu, delta, T, theta, r, x, F):
+    A, B, R= R_calculation(N, mu, delta,T,  r, F)
+    S=A/math.sqrt(B-A**2)
+    s_d_theta=0
+    F_d_theta=numpy.zeros(N)
     for t in range(1, T+1):
-        first_term= (S*(1+S^2)*A - S^3*R[t])/(A^2*T)
-        sgn =  numpy.sign(F[t] - F[t-1])
-        second_term = ( (-1)*mu*delta*sgn)*(1- numpy.tanh(numpy.dot(x[t],theta))^2)*(x[t] + theta[M+2]*F_derivated)+ ((-1)*R[t] + mu*delta*sgn*F_derivated)
-        s_derivated+= first_term*second_term
-        F_derivated =(1- numpy.tanh(numpy.dot(x[t],theta))^2)*(x[t] + theta[M+2]*F_derivated)
+        first_term= (S*(1+S**2)*A - S**3*R[t])/(A**2*T)
+        sgn =  numpy.sign(F[t,:] - F[t-1,:])
+        second_term = ( (-1)*mu*delta*sgn)*(1- F[t,:] @ F[t,:])*(x[t] +  theta[1+(M+1)*N :1+N*(M+2),: ]*F_d_theta)+ ((-1)*r[t]*mu + mu*delta*sgn)*F_d_theta)
+        s_d_theta+= first_term*second_term
+        F_d_theta =(1- F[t,] @ F[t,])*(x[t] + theta[1+(M+1)*N :1+N*(M+2), ]*F_d_theta)
 
     return s_derivated
 
-def R_calculation(mu, delta, T, price_T, F_T):
+
+#we know r[N] from the price data
+# CEO
+#besoin de T vecteurs r de r1 à rT
+#besoin de T vecteurs F de F1 à FT
+def R_calculation(N, mu, delta, T, r, F):
     A=0
     B=0
-    for t in range(1,T):
-        r_t = log(price_T[t]) - log(price_T[t-1])
-        R= mu*(F[t-1]*r[t]- delta*numpy.absolute(F[t]- F[t-1]))
-        A+= R
-        B+= R^2
-    return(A, B)
+    R= [0 for a in range(T)]
+    for t in range(1,T+1):
+        R_t= mu*(numpy.dot(F[t,:],r[t,:])- delta* numpy.linalg.norm((F[t,:]- F[t-1,:]), ord=1)
+        R[t]=R_t
+        A+= R[t]
+        B+= R[t]**2
+    return(A/T, B/T, R)
